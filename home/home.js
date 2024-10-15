8ba8a5a89a1a0347f121b972f76f0fa5fc951820
Adarsh-creater <adarshkumarggps@gmail.com>
2024-10-16 00:45:45 +0530

    update the home

---
+++ home/home.js
@@ -0,0 +1,40 @@
+let slideImages = document.querySelectorAll('.slides img');
+	
+
+	let counter = 0;
+
+
+	function slideNext(){
+	slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+	if(counter >= slideImages.length-1){
+		counter = 0;
+	}
+	else{
+		counter++;
+	}
+	slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+	// indicators();
+	}
+
+	
+
+	// Auto slideing
+	function autoSliding(){
+		deletInterval = setInterval(timer, 3000);
+		function timer(){
+			slideNext();
+			// indicators();
+		}
+	}
+	autoSliding();
+
+	// Stop auto sliding when mouse is over
+	const container = document.querySelector('.slide-container');
+	container.addEventListener('mouseover', function(){
+		clearInterval(deletInterval);
+	});
+
+	// Resume sliding when mouse is out
+	container.addEventListener('mouseout', autoSliding);
+
+	
